4282b7db804065012d99702a8789623b88af6efb
ruks2022 <107903447+ruks2022@users.noreply.github.com>
2022-06-27 12:38:12 +0530

    Todo App in node and express

---
+++ index.js
@@ -0,0 +1,57 @@
+//require the just installed express app
+var express = require('express');
+var bodyParser = require("body-parser");
+//then we call express
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+/**app.get('/', function (req, res) {
+//when we visit the root URL express will respond with 'Hello World'
+  //res.send('Hello Worl!');
+  res.render('index');
+});**/
+
+
+//the task array with initial placeholders for added task
+var task = ["buy socks", "practise with nodejs"];
+//post route for adding new task
+app.post('/addtask', function (req, res) {
+    var newTask = req.body.newtask;
+//add the new task from the post route into the array
+    task.push(newTask);
+//after adding to the array go back to the root route
+    res.redirect("/");
+});
+
+
+
+//the completed task array with initial placeholders for removed task
+var complete = ["finish jquery"];
+app.post("/removetask", function(req, res) {
+     var completeTask = req.body.check;
+//check for the "typeof" the different completed task, then add into the complete task
+if (typeof completeTask === "string") {
+     complete.push(completeTask);
+//check if the completed task already exist in the task when checked, then remove using the array splice method
+  task.splice(task.indexOf(completeTask), 1);
+} else if (typeof completeTask === "object") {
+    for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
+    task.splice(task.indexOf(completeTask[i]), 1);
+}
+}
+   res.redirect("/");
+});
+
+//render the ejs and display added task, task(index.ejs) = task(array)
+app.get("/", function(req, res) {    
+    res.render("index", { task: task, complete: complete });
+  });
+
+
+
+//the server is listening on port 3000 for connections
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
